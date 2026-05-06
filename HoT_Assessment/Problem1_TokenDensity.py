627240233c20d71b18180166371cdef3a680b116
Cheran10s <cherannithish10@gmail.com>
2026-05-06 10:58:37 +0530

    Delete HoT_Assessment/Problem1_TokenDensity.py

--- HoT_Assessment/Problem1_TokenDensity.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.DataFrame({"File":["A.c","A.c","B.c"],"Line":[1,2,1],"Token":["int","main","x"]})
-tpl = df.groupby(["File","Line"]).size().reset_index(name="Token_Count")
-density = tpl.groupby("File")["Token_Count"].mean().reset_index()
-density["Is_Token_Heavy"] = density["Token_Count"] > 2
-print(density)
