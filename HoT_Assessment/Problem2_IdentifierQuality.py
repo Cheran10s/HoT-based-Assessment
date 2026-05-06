d45ec2e10ca87c703a79e92cd3007ba93a4b8614
Cheran10s <cherannithish10@gmail.com>
2026-05-06 10:58:51 +0530

    Delete HoT_Assessment/Problem2_IdentifierQuality.py

--- HoT_Assessment/Problem2_IdentifierQuality.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.DataFrame({"File":["A.c","B.c"],"Identifier":["sum","x"]})
-df["Length"]=df["Identifier"].apply(len)
-avg=df.groupby("File")["Length"].mean().reset_index()
-avg["Is_Poor_Naming"]=avg["Length"]<3
-print(avg)
