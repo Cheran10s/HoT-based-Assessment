f142d8d7a1d042c3c1fc17934ab927b59b768e2e
Cheran10s <cherannithish10@gmail.com>
2026-05-06 11:00:01 +0530

    Delete HoT_Assessment/Problem6_ShiftReduce.py

--- HoT_Assessment/Problem6_ShiftReduce.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df=pd.DataFrame({"State":[1,1],"Conflict":[1,1]})
-conf=df.groupby("State")["Conflict"].sum().reset_index()
-conf["Is_Conflict_Prone"]=conf["Conflict"]>1
-print(conf)
